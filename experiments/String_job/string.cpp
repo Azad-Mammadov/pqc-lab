269149f1fab62f18691c1d9a8fb0d6f8cbee44f6
Azad Mammadov <45455149+Azad-Mammadov@users.noreply.github.com>
2025-06-20 23:16:34 +0200

    Update

--- experiments/String_job/string.cpp
+++ experiments/String_job/string.cpp
@@ -3,8 +3,19 @@
 
 int main()
 {
-    
-    
+
+    std::cout << " Enter 2 strings: \n";
+    std::string str1, str2;
+    std::cin >> str1 >> str2;
+    std::cout << "Here is the alphabetical order:  ";
+    if (str1 < str2) {
+        std::cout << str1 << " " << str2 << "\n";
+    } else if (str1 > str2) {
+        std::cout << str2 << " " << str1 << "\n";
+    } else {
+        std::cout << "Both strings are equal.\n";
+    }    return 0;
+
 }
 
 
